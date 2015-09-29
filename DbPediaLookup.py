efe49b51c45c7c015757a3fb6015b36aef21503b
Andrei Polzounov <and.pol5@gmail.com>
2015-09-29 16:16:25 +0200

    Fix crash on concatenating NoneType with string

--- DbPediaLookup.py
+++ DbPediaLookup.py
@@ -9,51 +9,43 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 import sys
 
-# This function checks if the given string redirects to a different dbpedia page
+# This function checks if the given string has a direct resource in dbpedia database
 # and returns the resource URL
-def getDbPediaResourceUrl(locationString, database):
-	database.setQuery("""
-		PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-		PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-		PREFIX dbo: <http://dbpedia.org/ontology/>
-
-		SELECT ?s WHERE {
-		{
-		?s rdfs:label \""""+ locationString.title() + """\"@en
-		# a dbo:Location
-
-		}
-		UNION
-		{
-		?altName rdfs:label \"""" + locationString.title() + """\"@en ;
-		dbo:wikiPageRedirects ?s .
-		}
-		}
-		""")
+def getDirectResourceUrl(locationString, database):
+	database.setQuery("""SELECT ?Dir WHERE {?Dir rdfs:label \"""" + locationString + """\"@en; a owl:Thing}""")
 	database.setReturnFormat(JSON)
 	results = database.query().convert()
-
 	for result in results["results"]["bindings"]:
-		return result["s"]["value"]
+		return result["Dir"]["value"]
+
+
+# This function checks if a string is a redirection 
+# and returns original resources URL
+def getRedirectedResourceURL(locationString, database):
+	database.setQuery("""SELECT ?Redir WHERE {dbr:""" + locationString + """ dbo:wikiPageRedirects ?Redir}""")
+	database.setReturnFormat(JSON)
+	results = database.query().convert()
+	for result in results["results"]["bindings"]:
+		return result["Redir"]["value"]
+
 
 # Function to query if a given resource has one of the below properties 
 # signifying that it is a location.
 def isLocation(resourceUrl, database):
-	if resourceUrl is None:
+	if resourceUrl is not None:
 		return None
+
 	database.setQuery("""
-		PREFIX db: <""" + resourceUrl + """>
-		PREFIX onto: <http://dbpedia.org/ontology/>
-	
 		SELECT COUNT(?o) AS ?NoOfResults
-		WHERE {db: a ?o
-		FILTER (?o = onto:PopulatedPlace OR
-		?o = onto:Place OR
-		?o = onto:Location OR
-		?o = onto:Settlement OR
-		?o = onto:Town OR
-		?o = onto:City OR
-		?o = onto:AdministrativeRegion)
+		WHERE {<""" + resourceUrl + """> a ?o
+		FILTER (?o = dbo:PopulatedPlace OR
+		?o = dbo:Place OR
+		?o = dbo:Location OR
+		?o = dbo:Settlement OR
+		?o = dbo:Town OR
+		?o = dbo:City OR
+		?o = dbo:AdministrativeRegion OR
+		?o = yago:Location100027167)
 		}
 		""")
 	database.setReturnFormat(JSON)
@@ -63,20 +55,64 @@ def isLocation(resourceUrl, database):
 		return result["NoOfResults"]["value"]
 
 
+# Function to query in case a location has any disambiguates.
+# If it does, the function counts all disambiguates, which have below properties.
+# If a function returns >0, this means the the string is a location
+def disambiguateCount(locationString, database):
+	if locationString is None:
+		return None
+	database.setQuery("""
+		select DISTINCT Count(?Disamb) as ?countOfDisambg  where 
+		{
+		dbr:""" + locationString + """ dbo:wikiPageDisambiguates ?Disamb .
+		?Disamb a ?types .
+		FILTER (?types = dbo:PopulatedPlace OR
+		?types  = dbo:Place OR
+		?types  = dbo:Location OR
+		?types  = dbo:Settlement OR
+		?types  = dbo:Town OR
+		?types  = dbo:City OR
+		?types  = dbo:AdministrativeRegion OR
+		?types  = yago:Location100027167)
+		}
+		""")
+	database.setReturnFormat(JSON)
+	results = database.query().convert()
+
+	for result in results["results"]["bindings"]:
+		return result["countOfDisambg"]["value"]
+
+
 userInput = raw_input("What location are you trying to check?\n")
 databaseWrapper = SPARQLWrapper("http://dbpedia.org/sparql")
 
 if len(userInput) > 1:
 	locationString = userInput
-	resourceUrl = getDbPediaResourceUrl(locationString, databaseWrapper)
+	locationString = locationString.strip()
+	locationString = locationString.title()
+
+	resourceUrl = getDirectResourceUrl(locationString, databaseWrapper) # Check for direct resource
+	print "Direct resource URL: ",
 	print resourceUrl
-	locationType = isLocation(resourceUrl, databaseWrapper)
-	if locationType is not None:
-		if int(locationType)>0:
-			print locationString + " is a location. (" + locationType + ")"
+
+	locationString = locationString.replace(" ", "_")
+	if resourceUrl is None:
+		resourceUrl = getRedirectedResourceURL(locationString, databaseWrapper) # Check for indirect resource
+		print "Redirected resource URL: ",
+		print resourceUrl
+	if resourceUrl is not None:
+		locationType = isLocation(resourceUrl, databaseWrapper) # Check if string is a location
+		if locationType is not None: 
+			if int(locationType) > 0:
+				print locationString + " is a location. (" + locationType + ")"
+			else:
+				print locationString + " is NOT a location"
 		else:
-			print locationString + " is NOT a location"
-	else:
-		print locationString + " is NOT a location"
+			disambCount = int(disambiguateCount(locationString, databaseWrapper)) # Check for disambiguates
+			if disambCount>0:
+				print "Disambiguates! (" + str(disambCount) + ")\n" + locationString + " is a location."
+			else:
+				print "No Disambiguates"
+				print locationString + " is NOT a location"
 else:
     print "Error: Enter a location"
