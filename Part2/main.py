a0d31e1aae0060fbdf730aa49e7484f39a62e992
Deividas <deividasskiparis@hotmail.com>
2015-12-30 20:03:38 +0100

    Update

--- Part2/main.py
+++ Part2/main.py
@@ -1,15 +1,24 @@
 from locationParser import parseQueries
 from XMLScorer import obtain_score
 # Test function to parse location queries and evaluate
+rFileName = None
+wFileName = None
+
 
 # Option 1: Provide file paths of xml files
 # rFileName = 'inputFileName.xml'
 # wFileName = 'outputFileName.xml'
 # parseQueries('write',readFileName=rFileName,writeFileName=wFileName)
 
-# Option 2: Don't provide file paths and use file dialogs to select files
-(rFileName,wFileName) = parseQueries('write')
+# Option 2: Don't provide file paths and instead use file dialogs to select files
+# (rFileName,wFileName) = parseQueries('write')
 
+# Option 3: Simply provide your own queries and only print results to terminal
+queries=['basketball court in petaling jaya','apartments to rent in cyprus']
+parseQueries('terminal',inputStr=queries)
+
+
+if wFileName == None:exit()
 
 # Calculate results:
 results = obtain_score(wFileName, 'GC_Test_Solved_100.xml',unsorted=True,usepaper=True)
